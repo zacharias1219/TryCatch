866161981bfc3a488d8f7c9e747fc206d6779f60
Richard Abishai <rickoshade1891@gmail.com>
2026-02-20 22:51:23 +0800

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -21,6 +21,25 @@ def main():
     chat = client.chat.completions.create(
         model="anthropic/claude-haiku-4.5",
         messages=[{"role": "user", "content": args.p}],
+        tools=[
+            {
+                "type": "function",
+                "function": {
+                    "name": "Read",
+                    "description": "Read and return the contents of a file",
+                    "parameters": {
+                        "type": "object",
+                        "properties": {
+                            "file_path": {
+                                "type": "string",
+                                "description": "The path to the file to read",
+                            }
+                        },
+                        "required": ["file_path"],
+                    },
+                },
+            }
+        ],
     )
 
     if not chat.choices or len(chat.choices) == 0:
