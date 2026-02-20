10f65997f89cc2167dad7ec396fc7d9b46b43482
Richard Abishai <rickoshade1891@gmail.com>
2026-02-20 22:55:35 +0800

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,12 +1,24 @@
 import argparse
 import os
-import json
 import sys
-
+import json
 from openai import OpenAI
 
 API_KEY = os.getenv("OPENROUTER_API_KEY")
 BASE_URL = os.getenv("OPENROUTER_BASE_URL", default="https://openrouter.ai/api/v1")
+
+
+def read(file_path: str) -> str:
+    """Read and return the contents of a file.
+
+    Args:
+        file_path: The path to the file to read
+    """
+    with open(file_path, "r") as f:
+        return f.read()
+
+
+TOOLS = {"read": read}
 
 
 def main():
@@ -19,42 +31,52 @@ def main():
 
     client = OpenAI(api_key=API_KEY, base_url=BASE_URL)
 
-    chat = client.chat.completions.create(
-        model="anthropic/claude-haiku-4.5",
-        messages=[{"role": "user", "content": args.p}],
-        tools=[{
-            "type": "function",
-            "function": {
-                "name": "Read",
-                "description": "Read and return the contents of a file",
-                "parameters": {
-                "type": "object",
-                "properties": {
-                    "file_path": {
-                    "type": "string",
-                    "description": "The path to the file to read"
+    messages = [{"role": "user", "content": args.p}]
+    while True:
+        chat = client.chat.completions.create(
+            model="anthropic/claude-haiku-4.5",
+            messages=messages,
+            tools=[
+                {
+                    "type": "function",
+                    "function": {
+                        "name": "read",
+                        "description": "Read and return the contents of a file",
+                        "parameters": {
+                            "type": "object",
+                            "properties": {
+                                "file_path": {
+                                    "type": "string",
+                                    "description": "The path to the file to read",
+                                }
+                            },
+                            "required": ["file_path"],
+                        },
+                    },
                 }
-            },
-            "required": ["file_path"]
-        }
-  }
-}],
-    )
+            ],
+        )
+        if not chat.choices or len(chat.choices) == 0:
+            raise RuntimeError("no choices in response")
 
-    if not chat.choices or len(chat.choices) == 0:
-        raise RuntimeError("no choices in response")
+        message = chat.choices[0].message
+        messages.append(message)
 
-
-    if chat.choices[0].message.tool_calls:
-        for tool_call in chat.choices[0].message.tool_calls:
-            if tool_call.function.name == "Read":
-                func_args = json.loads(tool_call.function.arguments)
-                file_path = func_args["file_path"]
-                with open (file_path, "r") as f:
-                    content = f.read()
-                print(content)
-    else:
-        print(chat.choices[0].message.content)
+        if message.tool_calls:
+            for tool_call in message.tool_calls:
+                fn = tool_call.function
+                function = fn.name
+                args = json.loads(fn.arguments)
+                messages.append(
+                    {
+                        "role": "tool",
+                        "tool_call_id": tool_call.id,
+                        "content": TOOLS[function](**args),
+                    }
+                )
+        if chat.choices[0].finish_reason == "stop":
+            break
+    print(chat.choices[0].message.content)
 
 
 if __name__ == "__main__":
